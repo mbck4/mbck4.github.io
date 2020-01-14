07aac81c024e152cfce0d51ab25934bc47d321e2
Use your GitHub username <Enter your email address>
2020-01-14 11:23:55 -0700

    assingment 1 updates

---
+++ scripts/currentDate.js
@@ -0,0 +1,7 @@
+var currentDate = new Date();
+console.log(currentDate);
+
+var currentYear = currentDate.getFullYear();
+console.log(currentYear);
+
+document.getElementById("currentYear").innerHTML = currentYear;
